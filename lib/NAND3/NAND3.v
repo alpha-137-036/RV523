2ce33e0f080850601daebf334ac70bb579aa403e
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-13 22:29:51 +0200

    same for NAND3

--- lib/NAND3/NAND3.v
+++ lib/NAND3/NAND3.v
@@ -8,32 +8,32 @@ module NAND3(
     supply0 GND;
     wire int1, int2;
 
-    NMOS N1(
+    RV523_NMOS N1(
         .S(GND),
         .D(int1),
         .G(A)
     );
-    NMOS N2(
+    RV523_NMOS N2(
         .S(int1),
         .D(int2),
         .G(B)
     );
-    NMOS N3(
+    RV523_NMOS N3(
         .S(int2),
         .D(Y),
         .G(C)
     );
-    PMOS P1(
+    RV523_PMOS P1(
         .S(VDD),
         .D(Y),
         .G(A)
     );
-    PMOS P2(
+    RV523_PMOS P2(
         .S(VDD),
         .D(Y),
         .G(B)
     );
-    PMOS P3(
+    RV523_PMOS P3(
         .S(VDD),
         .D(Y),
         .G(C)
