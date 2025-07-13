85692603b79f8b562b738458fddad69108699c27
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-13 22:36:10 +0200

    same for NAND

---
+++ lib/NAND/NAND.v
@@ -0,0 +1,30 @@
+module NAND(
+    output Y,
+    input A,
+    input B
+);
+    supply1 VDD;
+    supply0 GND;
+    wire int1;
+
+    RV523_NMOS N1(
+        .S(GND),
+        .D(int1),
+        .G(A)
+    );
+    RV523_NMOS N2(
+        .S(int1),
+        .D(Y),
+        .G(B)
+    );
+    RV523_PMOS P1(
+        .S(VDD),
+        .D(Y),
+        .G(A)
+    );
+    RV523_PMOS P2(
+        .S(VDD),
+        .D(Y),
+        .G(B)
+    );
+endmodule
