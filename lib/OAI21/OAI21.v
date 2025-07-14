74759e8527d7842222e56d6fff88ef0c9f3a264b
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-14 09:20:25 +0200

    OAI21 cell

---
+++ lib/OAI21/OAI21.v
@@ -0,0 +1,42 @@
+(* pins = "-,A,-,B1,-,B2,Y" *)
+module OAI21(
+    output Y,
+    input A,
+    input B1,
+    input B2
+);
+    supply1 VDD;
+    supply0 GND;
+    wire intB, intN;
+
+    RV523_NMOS N1(
+        .S(GND),
+        .D(intN),
+        .G(A)
+    );
+    RV523_NMOS N2(
+        .S(intN),
+        .D(Y),
+        .G(B1)
+    );
+    RV523_NMOS N3(
+        .S(intN),
+        .D(Y),
+        .G(B2)
+    );
+    RV523_PMOS P1(
+        .S(VDD),
+        .D(Y),
+        .G(A)
+    );
+    RV523_PMOS P2(
+        .S(VDD),
+        .D(intB),
+        .G(B1)
+    );
+    RV523_PMOS P3(
+        .S(intB),
+        .D(Y),
+        .G(B2)
+    );
+endmodule
