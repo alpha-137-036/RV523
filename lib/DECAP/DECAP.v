70401d7f9f64a087702a219d7aafa7b41e7bb098
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-15 12:06:27 +0200

    DECAP pseudo-cell

---
+++ lib/DECAP/DECAP.v
@@ -0,0 +1,18 @@
+
+(* blackbox *)
+(* footprint="Capacitor_SMD:C_0402_1005Metric" *)
+module CAP(
+    (* num="1" *)
+    inout pin1,
+    (* num="2" *)
+    inout pin2
+);
+endmodule
+
+module DECAP();
+    supply1 VDD;
+    supply0 GND;
+
+    (* keep *)
+    CAP C(.pin1(GND), .pin2(VDD));
+endmodule
