d04ca564e8c87dc2e6e28ccefc33a41916ded571
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-16 19:23:00 +0200

    D_LATCH now uses TINV

--- lib/D_LATCH/D_LATCH.v
+++ lib/D_LATCH/D_LATCH.v
@@ -1,35 +1,3 @@
-// Tristate inverter
-module TristateInverter(
-    output Y,
-    input  A,
-    input  EN,
-    input  nEN
-);
-    supply1 VDD;
-    supply0 GND;
-    wire intN, intP;
-    RV523_NMOS N1(
-        .S(GND),
-        .G(A),
-        .D(intN),
-    );
-    RV523_NMOS N2(
-        .S(intN),
-        .G(EN),
-        .D(Y),
-    );
-    RV523_PMOS P1(
-        .S(VDD),
-        .G(A),
-        .D(intP),
-    );
-    RV523_PMOS P2(
-        .S(intP),
-        .G(nEN),
-        .D(Y),
-    );
-endmodule
-
 
 // Elementary D-LATCH with complementary EN inputs
 module D_LATCH(
@@ -42,7 +10,7 @@ module D_LATCH(
     supply1 VDD;
     supply0 GND;
 
-    TristateInverter NOT1(
+    TINV NOT1(
         .A(D),
         .Y(nQ),
         .EN(EN),
@@ -52,7 +20,7 @@ module D_LATCH(
         .A(nQ),
         .Y(Q)
     );
-    TristateInverter NOT3(
+    TINV NOT3(
         .A(Q),
         .Y(nQ),
         .EN(nEN),
