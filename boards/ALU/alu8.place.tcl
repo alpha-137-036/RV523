678dcda79f9df358afcf3e1232e174ecbe7202e9
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-20 23:24:55 +0200

    Version script to autoplace using OpenROAD

---
+++ boards/ALU/alu8.place.tcl
@@ -0,0 +1,17 @@
+read_lef ../../lib/RV523.lef
+read_liberty ../../lib/RV523.lib
+read_verilog alu8.netlist.v
+link_design adder
+
+read_def -floorplan_initialize alu8_floorplan.def
+
+# For some reason place_pin crashes. 
+# Fortunately, the fixed pin locations in the .DEF file works
+
+
+#initialize_floorplan -die_area "0 0 100 100 " -core_area "0 0 50 70.4" -site CoreSite 
+
+global_placement -density 0.9 -routability_driven
+detailed_placement
+
+write_def alu8_top_placed.def
