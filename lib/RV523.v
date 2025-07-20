3461e3e5e815dc4778bc44902cf9eba3503931b5
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-20 23:15:33 +0200

    ALl forthcoming gates will have swappable pins with same letter and numbers

--- lib/RV523.v
+++ lib/RV523.v
@@ -12,13 +12,11 @@ module NAND2 (output Y, input A1, input A2);
     assign Y = ~(A1 & A2);
 endmodule
 
-// There is no advantage defining a primitive cell for AND: it will anyway be 
-// just a NAND followed by a NOT...
-
-module AND (output Y, input A, input B);
-    wire nY;
-    NAND u1(nY, A, B);
-    NOT u2(Y, nY);
+(* techmap_celltype = "AND2" *)
+(* blackbox *)
+(* footprint = "RV523:AND2" *)
+module AND2 (output Y, input A1, input A2);
+    assign Y = A1 & A2;
 endmodule
 
 (* techmap_celltype = "NOR2" *)
@@ -28,10 +26,11 @@ module NOR2 (output Y, input A1, input A2);
     assign Y = ~(A1 | A2);
 endmodule
 
-module OR (output Y, input A, input B);
-    wire nY;
-    NOR u1(nY, A, B);
-    NOT u2(Y, nY);
+(* techmap_celltype = "OR2" *)
+(* blackbox *)
+(* footprint = "RV523:OR2" *)
+module OR2 (output Y, input A1, input A2);
+    assign Y = A1 | A2;
 endmodule
 
 (* techmap_celltype = "NAND3" *)
@@ -41,11 +40,25 @@ module NAND3 (output Y, input A1, input A2, input A3);
     assign Y = ~(A1 & A2 & A3);
 endmodule
 
+(* techmap_celltype = "AND3" *)
+(* blackbox *)
+(* footprint = "RV523:AND3" *)
+module AND3 (output Y, input A1, input A2, input A3);
+    assign Y = A1 & A2 & A3;
+endmodule
+
 (* techmap_celltype = "NOR3" *) 
 (* blackbox *)
 (* footprint = "RV523:NOR3" *)
 module NOR3 (output Y, input A1, input A2, input A3);
     assign Y = ~(A1 | A2 | A3);
+endmodule
+
+(* techmap_celltype = "OR3" *)
+(* blackbox *)
+(* footprint = "RV523:OR3" *)
+module OR3 (output Y, input A1, input A2, input A3);
+    assign Y = A1 | A2 | A3;
 endmodule
 
 (* techmap_celltype = "AOI21" *)
