21407e80223a2fe206f5dffb04e024f25af0a8ad
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-07-23 22:21:33 +0200

    NOR4 cell

---
+++ lib/NOR4/generate_pwl.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
+import generate_pwl_generic
+
+generate_pwl_generic.VDD = 5
+
+generate_pwl_generic.generate_pwl("pwl_Inputs.sp", ["A1", "A2", "A3", "A4"])
