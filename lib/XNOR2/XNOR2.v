1ab068902eab76f771093d73474e4a847590196a
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-08-11 16:34:08 +0200

    10T XNOR2 gate

--- lib/XNOR2/XNOR2.v
+++ lib/XNOR2/XNOR2.v
@@ -1,19 +1,31 @@
-`include "../NOT/NOT.v"
-`include "../AOI22/AOI22.v"
+`include "../NAND2/NAND2.v"
+`include "../OAI21/OAI21.v"
 
 module XNOR2(
     output Y,
     input A,
     input B
 );
-    wire nA, nB;
-    NOT notA(.A(A), .Y(nA));    
-    NOT notB(.A(B), .Y(nB));
-    AOI22 aoi(
-        .A1(A),
-        .A2(nB),
-        .B1(nA),
-        .B2(B),
-        .Y(Y)
-    );
+    supply0 GND;
+    supply1 VDD;
+
+    wire sndNA, inand, nmid, sndPA;
+    
+    RV523_NMOS N1(.S(GND), .G(A), .D(sndNA));
+    RV523_NMOS N2(.S(sndNA), .G(B), .D(inand));
+    RV523_NMOS N3(.S(GND), .G(A), .D(nmid));
+    RV523_NMOS N4(.S(GND), .G(B), .D(nmid));
+    RV523_NMOS N5(.S(nmid), .G(inand), .D(Y));
+
+    RV523_PMOS P1(.S(VDD), .G(A), .D(inand));
+    RV523_PMOS P2(.S(VDD), .G(B), .D(inand));
+    RV523_PMOS P3(.S(VDD), .G(A), .D(sndPA));
+    RV523_PMOS P4(.S(sndPA), .G(B), .D(Y));
+    RV523_PMOS P5(.S(VDD), .G(inand), .D(Y));
+
+    // wire nAandB;
+    // NAND2 nand1(.A1(A), .A2(B), .Y(nAandB));
+    // OAI21 oai21(.A(nAandB), .B1(A), .B2(B), .Y(Y));
+
+
 endmodule
