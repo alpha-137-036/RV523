a83c3bdd9756af3275e3cd17483511343517d166
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-08-11 20:30:55 +0200

    10T XOR2 gate

--- lib/XOR2/XOR2.v
+++ lib/XOR2/XOR2.v
@@ -1,19 +1,24 @@
-`include "../NOT/NOT.v"
-`include "../AOI22/AOI22.v"
-
 module XOR2(
     output Y,
     input A,
     input B
 );
-    wire nA, nB;
-    NOT notA(.A(A), .Y(nA));    
-    NOT notB(.A(B), .Y(nB));
-    AOI22 aoi(
-        .A1(A),
-        .A2(B),
-        .B1(nA),
-        .B2(nB),
-        .Y(Y)
-    );
+
+    // Copied and adapted from https://github.com/google/skywater-pdk-libs-sky130_fd_sc_hd/blob/ac7fb61f06e6470b94e8afdf7c25268f62fbd7b1/cells/xor2/sky130_fd_sc_hd__xor2_1.cdl
+
+    wire inor, sndNA, sndPA, pmid;
+
+    RV523_NMOS N1(.S(GND), .D(inor), .G(A));
+    RV523_NMOS N2(.S(GND), .D(inor), .G(B));
+
+    RV523_PMOS P1(.S(VDD),   .D(sndPA), .G(A));
+    RV523_PMOS P2(.S(sndPA), .D(inor),  .G(B));
+
+    RV523_NMOS N3(.S(GND),   .D(sndNA), .G(A));
+    RV523_NMOS N4(.S(sndNA), .D(Y),     .G(B));
+    RV523_NMOS N5(.S(GND),   .D(Y),     .G(inor));
+
+    RV523_PMOS P3(.S(VDD),   .D(pmid), .G(A));
+    RV523_PMOS P4(.S(VDD),   .D(pmid), .G(B));
+    RV523_PMOS P5(.S(pmid),  .D(Y),    .G(inor));
 endmodule
