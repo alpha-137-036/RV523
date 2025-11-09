7b7b1248dce32db939ea7199fd34a01f7df7bfa6
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-11-09 16:45:51 +0100

    Placement of shifter1 macro

--- boards/ALU/Shifter/generate_shifter1_floorplan.py
+++ boards/ALU/Shifter/generate_shifter1_floorplan.py
@@ -4,21 +4,28 @@ BUSBITCHARS "[]" ;
 DESIGN shifter1 ;
 UNITS DISTANCE MICRONS 1000 ;
 DIEAREA ( 0 0 ) ( 100000 100000 ) ;""")
-for row in range(20):
-    print(f"""ROW ROW_{row} CoreSite 10000  {10000+row*4000} N DO 64 BY 1 STEP 1000 0 ;""")
-print("""TRACKS X 0 DO 1000 STEP 100 LAYER M1 ;
-TRACKS Y 0 DO 1000 STEP 100 LAYER M1 ;
-TRACKS X 0 DO 1000 STEP 100 LAYER M2 ;
-TRACKS Y 0 DO 1000 STEP 100 LAYER M2 ;
-TRACKS X 0 DO 1000 STEP 100 LAYER M3 ;
-TRACKS Y 0 DO 1000 STEP 100 LAYER M3 ;
-TRACKS X 0 DO 1000 STEP 100 LAYER M4 ;
-TRACKS Y 0 DO 1000 STEP 100 LAYER M4 ;
-PINS 0 ;""")
+for row in range(22):
+    print(f"""ROW ROW_{row} CoreSite 5000  {6000+row*4000} N DO 44 BY 1 STEP 1000 0 ;""")
+trackStep = 50
+trackCount = 2000
+for m in range(1,4):
+    print(f"""TRACKS X 0 DO {trackCount} STEP {trackStep} LAYER M{m} ;
+TRACKS Y 0 DO {trackCount} STEP {trackStep} LAYER M{m} ;
+    """)
+print("""PINS 0 ;""")
 x = 90000
 y = 67500
 pinDimension=300
 pinRect = f"( {-pinDimension/2} {-pinDimension/2} ) ( {pinDimension/2} {pinDimension/2} )"
+print(f"""- A0 + NET XXX 
+ + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
+y = y - 500
+print(f"""- rev1 + NET XXX 
+ + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
+y = y - 500
+print(f"""- rev1_n + NET XXX 
+ + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
+y = y - 500
 for i in range(32):
     print(f"""- A[{i}] + NET XXX 
      + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
@@ -30,15 +37,6 @@ for i in range(2):
     print(f"""- B[{i}] + NET XXX 
      + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
     y = y - 500
-print(f"""- A0 + NET XXX 
- + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
-y = y - 500
-print(f"""- rev1 + NET XXX 
- + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
-y = y - 500
-print(f"""- rev1_n + NET XXX 
- + USE SIGNAL + PORT + LAYER M1 {pinRect} + FIXED ( {x} {y} ) E ;""")
-y = y - 500
 print("""END PINS
 END DESIGN""")
 
--- boards/ALU/Shifter/shifter1.openroad.tcl
+++ boards/ALU/Shifter/shifter1.openroad.tcl
@@ -11,7 +11,7 @@ read_def -floorplan_initialize shifter1_floorplan.def
 
 
 #initialize_floorplan -die_area "0 0 100 100 " -core_area "0 0 50 70.4" -site CoreSite 
-global_placement -density 0.8 -routability_driven
+global_placement -density 0.85 -routability_driven
 detailed_placement -disallow_one_site_gaps 
 improve_placement -random_seed 1234
 improve_placement -random_seed 6666
@@ -21,9 +21,12 @@ improve_placement -random_seed 7777
 improve_placement -random_seed 4444
 improve_placement -random_seed 2222
 improve_placement -random_seed 1111
+improve_placement -random_seed 9876
+improve_placement -random_seed 911
 
 write_def shifter1_placed.def
 
-pin_access
-global_route
-detailed_route
+#pin_access -via_in_pin_bottom_layer M1
+
+#global_route -verbose
+#detailed_route  -via_in_pin_bottom_layer M1
