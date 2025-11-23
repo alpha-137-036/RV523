d5114242536ae3a47a471646cd703dd11d92e5ca
alpha-137-036 <215357397+alpha-137-036@users.noreply.github.com>
2025-11-23 21:27:06 +0100

    Simulate data memory

--- CPU/test/CPU_tb.sv
+++ CPU/test/CPU_tb.sv
@@ -1,12 +1,59 @@
 
 `timescale 1ns/1ns
 
+module code_rom(
+    input logic [31:2] addr,
+    
+    output logic [31:0] rdata
+);
+    logic [31:0] code[0:1023];
+    
+    initial begin
+        $readmemh("code.hex", code);
+    end
+    
+    always_comb begin
+        rdata = code[addr];
+    end
+endmodule
+
+module ram(
+    input  logic [31:0] d_addr,
+    output logic [31:0] d_rdata,
+    input  logic [31:0] d_wdata,
+    input  logic        d_read,
+    input  logic        d_write
+);
+    logic [31:0] data[0:1023];
+    
+    initial begin
+        integer i;
+        for (i = 0; i < 1024; i++) begin
+            data[i] = 0;
+        end
+    end
+    
+    always_comb begin
+        if (d_read) begin
+            d_rdata = data[d_addr - 32'h20000000];
+        end
+        if (d_write) begin
+            data[d_addr - 32'h20000000] = d_wdata;
+        end
+    end
+endmodule
+
 module CPU_tb();
 
     logic clk;
     logic rst_n;
     logic [31:0] c_addr;
     logic [31:0] c_rdata;
+    logic [31:0] d_addr;
+    logic [31:0] d_rdata;
+    logic [31:0] d_wdata;
+    logic        d_read;
+    logic        d_write;
     
     initial begin
         clk = 1;
@@ -44,11 +91,24 @@ module CPU_tb();
         .addr(c_addr[31:2]),
         .rdata(c_rdata)
     );
+    
+    ram u_ram(
+        .d_addr(d_addr),
+        .d_read(d_read),
+        .d_write(d_write),
+        .d_rdata(d_rdata),
+        .d_wdata(d_wdata)
+    );
 
     CPU u_cpu(
         .clk(clk),
         .rst_n(rst_n),
         .c_addr(c_addr),
-        .c_rdata(c_rdata)
+        .c_rdata(c_rdata),
+        .d_addr(d_addr),
+        .d_read(d_read),
+        .d_write(d_write),
+        .d_rdata(d_rdata),
+        .d_wdata(d_wdata)
     );
 endmodule
--- CPU/test/test.sh
+++ CPU/test/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 set -e
-iverilog -g2012 -s CPU_tb ../rtl/IF.sv ../rtl/Regs.sv ../rtl/ID.sv ../rtl/EX.sv ../rtl/ALU.sv ../rtl/CPU.sv CodeROM.sv CPU_tb.sv -o CPU_tb
+iverilog -g2012 -s CPU_tb ../rtl/IF.sv ../rtl/Regs.sv  ../rtl/ALU.svh ../rtl/ID.sv ../rtl/EX.sv ../rtl/ALU.sv ../rtl/MEM.sv ../rtl/WB.sv ../rtl/CPU.sv CPU_tb.sv -o CPU_tb
 vvp -n CPU_tb
